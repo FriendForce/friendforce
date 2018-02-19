88fce79ae79bd351ac1687702bbf762f873c92be
Adrienne Tran <adrienne@adriennetran.com>
2018-02-19 15:23:30 -0800

    firebase

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import List from './components/List.jsx';
+import * as firebase from "firebase";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    var config = {
+      apiKey: "AIzaSyCMO50yQLEyIw_u6aptgBmK3qsRmhpUjxQ",
+      authDomain: "friendforce-25851.firebaseapp.com",
+      databaseURL: "https://friendforce-25851.firebaseio.com",
+      storageBucket: "friendforce-25851.appspot.com",
+    };
+    firebase.initializeApp(config);
+
     this.state = { 
       items: []
     }
