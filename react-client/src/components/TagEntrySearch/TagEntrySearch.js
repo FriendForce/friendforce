ca15919f4cb0970013c58eaca9ecc2881e797f48
bzreinhardt <bzreinhardt@gmail.com>
2018-03-17 17:30:56 -0700

    working interface with nodes and edges

--- react-client/src/components/TagEntrySearch/TagEntrySearch.js
+++ react-client/src/components/TagEntrySearch/TagEntrySearch.js
@@ -1,17 +1,28 @@
-import styles from './TagEntrySearch.less'
+import styles from './TagEntrySearch.less';
 import React, { Component } from 'react';
 import Entry from './components/Entry/Entry.js';
 import Tags from './components/Tags/Tags.js';
 import data from './infos';
+import people from './people';
+import edges from './edges';
 // TODO need to pass getter and setter functions
 
 export default class TagEntrySearch extends Component {
   constructor() {
     super();
     this.state = {
-      data: data
+      data: data,
+      edges: edges,
+      people: people,
+      current_person: {},
+      user: {
+        id: '4',
+        name: 'Trinity'
+      }
     };
     this.addInfo = this.addInfo.bind(this);
+    this.addPerson = this.addPerson.bind(this);
+    this.addEdge = this.addEdge.bind(this);
   }
 
   addInfo = info => {
@@ -21,12 +32,43 @@ export default class TagEntrySearch extends Component {
     console.log('updated Info');
   };
 
+  addPerson = person => {
+    //TODO - weird bug where it doesn't push a person until a new person
+    // is entered
+    var temp_array = this.state.people.slice();
+    temp_array.push(person);
+    this.setState({ people: temp_array });
+    console.log('updated people');
+  };
+
+  addEdge = edge => {
+    var temp_array = this.state.edges.slice();
+    temp_array.push(edge);
+    this.setState({ edges: temp_array });
+    console.log('updated edges');
+  };
+
   render() {
     return (
       <div className={styles.container}>
-        <h2 >Examples</h2>
-        <Tags infos={this.state.data} addInfo={this.addInfo} />
-        <Entry infos={this.state.data} addInfo={this.addInfo} />
+        <h2 className={styles.header}>Examples</h2>
+        <Tags
+          infos={this.state.data}
+          people={this.state.people}
+          edges={this.state.edges}
+          addInfo={this.addInfo}
+          addPerson={this.addPerson}
+          addEdge={this.addEdge}
+        />
+        <Entry
+          infos={this.state.data}
+          people={this.state.people}
+          edges={this.state.edges}
+          user={this.state.user}
+          addInfo={this.addInfo}
+          addPerson={this.addPerson}
+          addEdge={this.addEdge}
+        />
       </div>
     );
   }
---
+++ react-client/src/components/TagEntrySearch/people.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: '1',
+    name: 'Adrienne Tran'
+  },
+  {
+    id: '2',
+    name: 'Micah Catlin'
+  },
+  {
+    id: '3',
+    name: 'Benjamin Reinhardt'
+  }
+];
