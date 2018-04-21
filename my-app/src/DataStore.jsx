29ddeddc088be887d702e50498f1b980aa613ec0
bzreinhardt <bzreinhardt@gmail.com>
2018-04-21 15:50:21 -0700

    remove uuid function (unused)

--- my-app/src/DataStore.jsx
+++ my-app/src/DataStore.jsx
@@ -3,15 +3,6 @@
 import persons from './ConstData/persons.js';
 import tags from './ConstData/tags.js';
 
-export const uuid = foo => {
-  // Get a random uuid
-  // This is temporary until the database is hooked up
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-  // eslint-disable-next-line
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  )
-}
-
 export const nameToId = name => {
   return name.replace(/[^A-Z0-9]/ig, "_") + crypto.getRandomValues(new Uint8Array(1));
 }
