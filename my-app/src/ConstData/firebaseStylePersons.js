8e01624f0b5914961eb2791032468c96cd4ffb58
Micah Catlin <micah.catlin@gmail.com>
2018-05-19 14:08:46 -0700

    Thoroughly change test data to be instaces

--- my-app/src/ConstData/firebaseStylePersons.js
+++ my-app/src/ConstData/firebaseStylePersons.js
@@ -1,29 +1,36 @@
 import Person from '../Types/Person'
 
 export default {
- 'Adrienne_Tran': new Person('Adrienne_Tran', 'Adrienne Tran', [ 'Micah_Catlin', 'Benjamin_Reinhardt' ]),
- 'Micah_Catlin': {
-    id: 'Micah_Catlin',
-    name: 'Micah Catlin',
-    knownByPersons: [
+  'Adrienne_Tran': new Person(
+    'Adrienne_Tran',
+    'Adrienne Tran',
+    [
       'Micah_Catlin',
       'Benjamin_Reinhardt'
     ]
-  },
-  'Benjamin_Reinhardt':{
-    id: 'Benjamin_Reinhardt',
-    name: 'Benjamin Reinhardt',
-    knownByPersons: [
+  ),
+  'Micah_Catlin': new Person(
+    'Micah_Catlin',
+    'Micah Catlin',
+    [
+      'Micah_Catlin',
+      'Benjamin_Reinhardt'
+    ]
+  ),
+  'Benjamin_Reinhardt': new Person(
+    'Benjamin_Reinhardt',
+    'Benjamin Reinhardt',
+    [
       'Micah_Catlin',
       'Benjamin_Reinhardt',
       'Sasha_Sheng'
     ]
-  },
-    'Sasha_Sheng':{
-    id: 'Sasha_Sheng',
-    name: 'Sasha Sheng',
-    knownByPersons: [
+  ),
+  'Sasha_Sheng': new Person(
+    'Sasha_Sheng',
+    'Sasha Sheng',
+    [
       'Benjamin_Reinhardt'
     ]
-  }
+  )
 };
