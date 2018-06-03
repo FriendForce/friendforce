dc072c96659e8162728e7d639a6d03a366e8630f
Paul Sawaya <me@paulsawaya.com>
2018-06-03 12:04:22 -0700

    fix person init bug, refactor types/Person

--- my-app/src/Types/Person.js
+++ my-app/src/Types/Person.js
@@ -13,21 +13,17 @@ export default class Person {
      * @param name {object} data to initialize the person 
      * @return {Person}
      */
-    if (typeof(name) === 'string') {
-      this.id = id;
-      this.name = name;
-      this.knownByPersons = {};
-    }
+    this.id = id;
+    this.name = name || '';
+    this.knownByPersons = {};
     if(typeof(name) === 'object') {
       // initialized from a databse object
-      this.id = id;
-      this.name = '';
       this.knownByPersons = {};
       for (var prop in name) {
         if (name.hasOwnProperty(prop)) {
           this[prop] = name[prop];
         }
       } 
-    } 
+    }
   }
 }
