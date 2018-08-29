c817b8c47aba60a55610475dfe5672066d0df79c
bzreinhardt <bzreinhardt@gmail.com>
2018-08-29 14:47:06 -0700

    upload json files

--- my-app/src/Onboarding/Onboarding.js
+++ my-app/src/Onboarding/Onboarding.js
@@ -27,24 +27,47 @@ export default class Onboarding extends Component {
     const dontSync = true;
     var fr = new FileReader();
     fr.onload = e => {
-      var parser = new DOMParser();
-      var htmlDoc = parser.parseFromString(e.target.result, 'text/html');
-      var friend_html = htmlDoc.body.children[1].children[2].children;
-      //for (var i = 0; i < friend_html.length; i++) {
-      for (var i = 0; i < 1; i++) {
-        // TODO: need to handle corner case where person has >2 names
-        var name = friend_html[i].innerText.split(' (')[0];
-        const addedDate = facebookDateToDate(
-          friend_html[i].innerText.split(' (')[1].split(')')[0]
-        );
-        // Create a person for each person
-        this.props.createPerson(name).then(id => {
-          var tag_text = 'datemetfb:' + addedDate;
-          this.props.createTag(tag_text, id, 'private');
+      const filename = files[0].name;
+      const type = filename.split('.')[filename.split('.').length - 1];
+
+      if (type === 'json') {
+        const friends = JSON.parse(e.target.result).friends;
+        friends.forEach(friend => {
+          this.props.createPerson(friend.name).then(id => {
+            var tag_text = 'datemetfb:' + friend.timestamp;
+            this.props.createTag(tag_text, id, 'private');
+            if (friend.contact_info != null) {
+              this.props.createTag(
+                'email:' + friend.contact_info,
+                id,
+                'private'
+              );
+            }
+          });
         });
+        this.props.updateAccount({ connected_facebook: true });
       }
-      this.props.updateAccount({ connected_facebook: true });
+      if (type === 'html') {
+        var parser = new DOMParser();
+        var htmlDoc = parser.parseFromString(e.target.result, 'text/html');
+        var friend_html = htmlDoc.body.children[1].children[2].children;
+        //for (var i = 0; i < friend_html.length; i++) {
+        for (var i = 0; i < 1; i++) {
+          // TODO: need to handle corner case where person has >2 names
+          var name = friend_html[i].innerText.split(' (')[0];
+          const addedDate = facebookDateToDate(
+            friend_html[i].innerText.split(' (')[1].split(')')[0]
+          );
+          // Create a person for each person
+          this.props.createPerson(name).then(id => {
+            var tag_text = 'datemetfb:' + addedDate;
+            this.props.createTag(tag_text, id, 'private');
+          });
+        }
+        this.props.updateAccount({ connected_facebook: true });
+      }
     };
+
     fr.readAsText(files[0]);
   };
 
@@ -53,16 +76,17 @@ export default class Onboarding extends Component {
       <div>
         <div>
           {' '}
-          If you have already downloaded your facebook data (friends.htm), load
+          If you have already downloaded your facebook data (friends.json), load
           it here: This May Take a While. Please Be Patient.
         </div>
 
         <div className="input-group">
           <label className="input-group-btn">
             <span className="btn btn-primary">
-              Upload Facebook Data Folder&hellip;{' '}
+              Upload friends.json{' '}
               <input
                 type="file"
+                placeholder="friends.json"
                 style={{ display: 'none' }}
                 multiple
                 onChange={e => this.uploadFBData(e.target.files)}
@@ -205,8 +229,8 @@ export default class Onboarding extends Component {
               alt="Step Nine"
             />
             <figcaption className="figure-caption">
-              Step 9 of 9: Click "Upload Facebook Data" and navigate to facebook
-              data folder=>html=>friends.htm
+              Step 9 of 9: Click "Upload friends.json" and navigate to facebook
+              data folder=>friends.json
             </figcaption>
           </figure>
         </div>
@@ -216,8 +240,9 @@ export default class Onboarding extends Component {
           <div className="input-group">
             <label className="input-group-btn">
               <span className="btn btn-primary">
-                Upload Facebook friends.htm&hellip;{' '}
+                Upload friends.json{' '}
                 <input
+                  placeholder="friends.json"
                   type="file"
                   name="files"
                   style={{ display: 'none' }}
